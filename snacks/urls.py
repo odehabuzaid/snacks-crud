8e92745018e4f03914ded2bbfa332ec531ff40c1
Odeh <abuzaid163@hotmail.com>
2021-11-16 21:58:49 +0200

    feat(snack): add  details view

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import SnackListView
+from .views import SnackDetailView, SnackListView
 
 urlpatterns = [
     path("", SnackListView.as_view(), name="snack_list"),
-    
+    path("<int:pk>/", SnackDetailView.as_view(), name="snack_details"),
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 from .models import Snack
 
@@ -7,3 +7,8 @@ class SnackListView(ListView):
     template_name = "snack_list.html"
     context_object_name = "snack_list"
     
+class SnackDetailView(DetailView):
+    model = Snack
+    template_name = "snack_details.html"
+    context_object_name = "snack_details"
+
