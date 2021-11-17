9a321c1e3198c89160ecd67c85f20c0c0359c109
Odeh <abuzaid163@hotmail.com>
2021-11-17 05:46:33 +0200

    ci: testing links

--- snacks/models.py
+++ snacks/models.py
@@ -6,7 +6,10 @@ from django.urls import reverse
 class Snack(models.Model):
     title = models.CharField(max_length=256, help_text="Enter Snack Name")
     purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    discription = models.TextField()
-    
+    description = models.TextField()
+
     def get_absolute_url(self):
         return reverse("snack_details", args=[str(self.id)])
+
+    def __str__(self):
+        return self.title
